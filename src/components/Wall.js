fd00e9acd86043a2c141d69a9e2479dbe545ebca
Laura Lugo <lauri.lugo7@gmail.com>
2023-07-11 22:47:07 -0500

    Corrección ajuste de linea y textarea

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -80,122 +80,125 @@ export const Wall = (onNavigate) => {
         },
       );
       postOrderer.sort((a, b) => b.date - a.date);
-      // console.log(postOrderer);
-      postOrderer.forEach((postUser) => {
-        const postDiv = document.createElement('div');
-        postDiv.className = 'post-div';
-        const postText = document.createElement('input');
-        postText.value = postUser.publication;
-        postText.disabled = true;
+      console.log(postOrderer);
+    });
+    postOrderer.forEach((postUser) => {
+      const postDiv = document.createElement('div');
+      postDiv.className = 'post-div';
+      const postText = document.createElement('textarea');
+      postText.className = 'post-text';
+      postText.value = postUser.publication;
+      postText.disabled = true;
 
-        const buttonLikeImg = document.createElement('img');
-        buttonLikeImg.src = likeIcon;
+      const buttonLikeImg = document.createElement('img');
+      buttonLikeImg.src = likeIcon;
 
-        const buttonLike = document.createElement('button');
-        buttonLike.textContent = `${postUser.likes.length}`;
-        buttonLike.addEventListener('click', () => {
-          const user = emailUsuario();
-          console.log(user);
-          const userLiked = postUser.likes.includes(user);
-          if (!userLiked) {
-            postUser.likes.push(user);
-            likePost(postUser.id, postUser.likes).then(() => {
-              onNavigate('/wall');
-            });
-          }
-        });
-
-        buttonLike.appendChild(buttonLikeImg);
-
-        const editIconImg = document.createElement('img');
-        editIconImg.src = editIcon;
-
-        const buttonEdit = document.createElement('button');
-        buttonEdit.alt = 'Editar';
-        buttonEdit.addEventListener('click', () => {
-          postText.disabled = !postText.disabled;
-          if (postText.disabled) {
-            postText.style.border = 'none';
-          } else {
-            postText.style.border = '1px solid #000000';
-          }
-        });
-
-        buttonEdit.appendChild(editIconImg);
-
-        const saveIconImg = document.createElement('img');
-        saveIconImg.src = saveIcon;
-
-        const buttonSave = document.createElement('button');
-        buttonSave.alt = 'Guardar';
-        buttonSave.addEventListener('click', () => {
-          updatePost(post.id, postText.value).then(() => {
+      const buttonLike = document.createElement('button');
+      buttonLike.textContent = `${postUser.likes.length}`;
+      buttonLike.addEventListener('click', () => {
+        const user = emailUsuario();
+        console.log(user);
+        const userLiked = postUser.likes.includes(user);
+        if (!userLiked) {
+          postUser.likes.push(user);
+          likePost(postUser.id, postUser.likes).then(() => {
             onNavigate('/wall');
           });
-        });
-
-        buttonSave.appendChild(saveIconImg);
-
-        const buttonDeleteImg = document.createElement('img');
-        buttonDeleteImg.src = deleteIcon;
-
-        const buttonDelete = document.createElement('button');
-        buttonDelete.alt = 'Borrar';
-        buttonDelete.addEventListener('click', () => {
-          const ConfirmationDiv = document.createElement('div');
-          ConfirmationDiv.setAttribute('class', 'confirmation-content');
-          wrapper.classList.toggle('window-active');
-
-          ConfirmationDiv.innerHTML = `
-            <div id='modal' class='modal'>
-              <p>¿Estás seguro de borrar este post?</p>
-              <div class='container-confirmationBts'>
-                <button id='buttonYes' class='buttonEdit'><img src="../media/accept.svg" alt="Aceptar" class='modal-button'></button>
-                <button id='buttonNo' class='buttonEdit'><img src="../media/denied.svg" alt="Cancelar" class='modal-button'></button>
-              </div>
-            </div>`;
-
-          document.body.appendChild(ConfirmationDiv);
-          // Agrega el evento click al botón de confirmar del modal
-          document.getElementById('buttonYes').addEventListener('click', async () => {
-            // Realizar la eliminación del post
-            try {
-              await deletePost(post.id);
-              onNavigate('/wall');
-              wrapper.classList.toggle('window-active');
-            } catch (error) {
-              console.error('Error deleting post:', error);
-            }
-            // Ocultar el div modal
-            ConfirmationDiv.style.display = 'none';
-            document.body.removeChild(ConfirmationDiv);
-          });
-
-          // Agrega el evento click al botón de cancelar del modal
-          document.getElementById('buttonNo').addEventListener('click', () => {
-            // Ocultar el div modal
-            ConfirmationDiv.style.display = 'none';
-            document.body.removeChild(ConfirmationDiv);
-            wrapper.classList.toggle('window-hidden');
-          });
-        });
-
-        buttonDelete.appendChild(buttonDeleteImg);
-
-        const buttonsWrapper = document.createElement('div');
-        buttonsWrapper.className = 'buttons-wrapper';
-
-        buttonsWrapper.appendChild(buttonLike);
-        buttonsWrapper.appendChild(buttonEdit);
-        buttonsWrapper.appendChild(buttonSave);
-        buttonsWrapper.appendChild(buttonDelete);
-
-        postDiv.appendChild(postText);
-        postDiv.appendChild(buttonsWrapper);
-        postContainer.appendChild(postDiv);
-
-        postWrapper.appendChild(postDiv);
+        }
       });
+
+      buttonLike.appendChild(buttonLikeImg);
+
+      const editIconImg = document.createElement('img');
+      editIconImg.src = editIcon;
+
+      const buttonEdit = document.createElement('button');
+      buttonEdit.alt = 'Editar';
+      buttonEdit.addEventListener('click', () => {
+        postText.disabled = !postText.disabled;
+        if (postText.disabled) {
+          postText.style.border = 'none';
+        } else {
+          postText.style.border = '1px solid #000000';
+        }
+      });
+
+      buttonEdit.appendChild(editIconImg);
+
+      const saveIconImg = document.createElement('img');
+      saveIconImg.src = saveIcon;
+
+      const buttonSave = document.createElement('button');
+      buttonSave.alt = 'Guardar';
+      buttonSave.addEventListener('click', () => {
+        updatePost(postUser.id, postText.value).then(() => {
+          onNavigate('/wall');
+        });
+      });
+
+      buttonSave.appendChild(saveIconImg);
+
+      const buttonDeleteImg = document.createElement('img');
+      buttonDeleteImg.src = deleteIcon;
+
+      const buttonDelete = document.createElement('button');
+      buttonDelete.alt = 'Borrar';
+      buttonDelete.addEventListener('click', () => {
+        const ConfirmationDiv = document.createElement('div');
+        ConfirmationDiv.setAttribute('class', 'confirmation-content');
+        wrapper.classList.toggle('window-active');
+
+        ConfirmationDiv.innerHTML = `
+          <div id='modal' class='modal'>
+            <p>¿Estás seguro de borrar este post?</p>
+            <div class='container-confirmationBts'>
+              <button id='buttonYes' class='buttonEdit'><img src="../media/accept.svg" alt="Aceptar" class='modal-button'></button>
+              <button id='buttonNo' class='buttonEdit'><img src="../media/denied.svg" alt="Cancelar" class='modal-button'></button>
+            </div>
+          </div>`;
+
+        document.body.appendChild(ConfirmationDiv);
+        // Agrega el evento click al botón de confirmar del modal
+        document.getElementById('buttonYes').addEventListener('click', async () => {
+          // Realizar la eliminación del post
+          try {
+            await deletePost(postUser.id);
+            onNavigate('/wall');
+            wrapper.classList.toggle('window-active');
+          } catch (error) {
+            console.error('Error deleting post:', error);
+          }
+          // Ocultar el div modal
+          ConfirmationDiv.style.display = 'none';
+          document.body.removeChild(ConfirmationDiv);
+        });
+
+        // Agrega el evento click al botón de cancelar del modal
+        document.getElementById('buttonNo').addEventListener('click', () => {
+          // Ocultar el div modal
+          ConfirmationDiv.style.display = 'none';
+          document.body.removeChild(ConfirmationDiv);
+          wrapper.classList.toggle('window-hidden');
+        });
+      });
+
+      buttonDelete.appendChild(buttonDeleteImg);
+
+      const buttonsWrapper = document.createElement('div');
+      buttonsWrapper.className = 'buttons-wrapper';
+
+      buttonsWrapper.appendChild(buttonLike);
+      buttonsWrapper.appendChild(buttonEdit);
+      buttonsWrapper.appendChild(buttonSave);
+      buttonsWrapper.appendChild(buttonDelete);
+
+      postDiv.appendChild(postText);
+      postDiv.appendChild(buttonsWrapper);
+      postContainer.appendChild(postDiv);
+
+      postWrapper.appendChild(postDiv);
+      postText.style.height = `${postText.scrollHeight}px`;
+      postDiv.style.height = `${postText.scrollHeight + 75}px`;
     });
   });
 
